f038e1e4d34a2a8a82e06c6a5230763a79f64c18
kirthikhavelusamy <kirthikha.velusamy@abstract-tech.com>
2022-05-31 00:13:00 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
+import Table from './Table';
+import Form from './Form';
 function App(){
-  return (<div>
-    {2+2}
-    <br/>
-    {"Kirthikha"}
-    <br/>
-    {true}{false}
-    <br/>
-    {2>5?"yes":"no"}
-    <br/>
-    {2>1 && 2<5 && "success"}
-    <br/>
-    {"record" || "yes"}
-    <p>Hello world</p>
-    <input type="text"/>
-  </div>)
+   return (<div>
+     <Table/>
+     <Form/>
+   </div>)
 }
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,4 @@
+function Table(){
+    return(<div>Table</div>)
+}
+export default Table;
